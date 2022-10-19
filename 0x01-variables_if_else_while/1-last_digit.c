57efa4101699b3766fe1c29dddfa7f80c0260204
jamesgotech <jameschibey@gmail.com>
2022-10-18 20:27:24 -0700

    fixed betty-docs and whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,16 +3,13 @@
 #include <stdio.h>
 
 /**
- *main - checks if the last digit is greater than 5, is 0, and is less than 6
- *and not 0
+ * main - Determines either greater than 5, is less than 6, or is 0
  *
- * return: always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
-	int l;
+	int n, l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
