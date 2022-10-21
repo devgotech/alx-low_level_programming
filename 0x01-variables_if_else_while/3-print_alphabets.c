0753b90648652e0e59b19d38c5d11b877ec0063d
jamesgotech <jameschibey@gmail.com>
2022-10-21 06:10:10 -0700

    Betty doc correction on 3-print_alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 /**
-  *main - prints lowercase followed by uppercase alphabets
-  *
-  *return alway 0 (success)
-  */
-
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{	
-	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+{
+	char alphaq[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i=0; i<52; i++)
+	for (i = 0; i < 52; i++)
 	{
-		putchar(alpha[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
 }
-
