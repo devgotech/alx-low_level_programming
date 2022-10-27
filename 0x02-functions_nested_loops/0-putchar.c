1155d475d4318152383fda69b3e26ba16d19a40c
jamesgotech <jameschibey@gmail.com>
2022-10-27 11:20:56 +0100

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Print _putchar as output
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-    int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-    int count, size;
+    char str[] = "_putchar";
+    int count;
 
-    size = sizeof(str) / sizeof(int);
-    for (count = 0; count < size; count++)
+    for (count = 0; count < 8; ++count)
     {
         _putchar(str[count]);
     }
     _putchar('\n');
+
     return (0);
 }
