e6c25062abc93e64bfbe9385628bcd71cc8f4fb7
jamesgotech <jameschibey@gmail.com>
2022-10-27 03:45:40 -0700

    betty-code _putchar fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-    char str[] = "_putchar";
-    int count;
+	char str[] = "_putchar";
+	int count;
 
-    for (count = 0; count < 8; ++count)
-    {
-        _putchar(str[count]);
-    }
-    _putchar('\n');
+	for (count = 0; count < 8; ++count)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
