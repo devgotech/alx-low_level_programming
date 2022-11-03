83507842cb94c2c893807c6801781a7ec07014c3
jamesgotech <jameschibey@gmail.com>
2022-11-03 11:49:54 +0100

    main.h editted

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
