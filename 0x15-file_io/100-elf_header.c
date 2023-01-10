aa9e05a3bd77b8ceee6ff9f968b4b271c7704939
jamesgotech <jameschibey@gmail.com>
2023-01-10 13:48:07 +0100

    elf err

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,11 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 
 /*
  * e_ident[] - IDENTIFICATION INDEXES
